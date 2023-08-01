8207d642f74188a3281ce269c4be2cb429888b85
mond gatacelo <raymundo.gatacelo@ing.com>
2023-08-02 00:42:11 +0800

    add code for eventhub trigger function

--- azChallengeFA-Customer/Functions/EventHubTriggerFunction.cs
+++ azChallengeFA-Customer/Functions/EventHubTriggerFunction.cs
@@ -5,17 +5,36 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
 using Azure.Messaging.EventGrid;
+using azChallengeFA_Customer.services;
+using System.Threading.Tasks;
 
 namespace azChallengeFA_Customer.Functions
 {
-    public static class EventHubTriggerFunction
+    public  class EventHubTriggerFunction
     {
+        private readonly ILogger<EventHubTriggerFunction> _logger;
+        private readonly ICustomerService _customerService;
+
+        public EventHubTriggerFunction(ILogger<EventHubTriggerFunction> logger, ICustomerService customerService)
+        {
+                _customerService = customerService;
+                _logger = logger;
+        }
+
         [FunctionName("EventHubTriggerFunction")]
-        public static void Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
+        public async Task Run([EventGridTrigger]EventGridEvent eventGridEvent, ILogger log)
         {
             log.LogInformation(eventGridEvent.Data.ToString());
 
+            try
+            {
+                await _customerService.PublishEventAsync(eventGridEvent.Data);
+            }
 
+            catch (Exception ex)
+            {
+                _logger.LogError($"An error occured: {ex.StackTrace}");
+            }
         }
     }
 }
