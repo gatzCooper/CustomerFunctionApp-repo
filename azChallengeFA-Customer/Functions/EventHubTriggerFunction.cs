6df55401d2d16af1f1411d9120e041309dcd63ec
mond gatacelo <raymundo.gatacelo@ing.com>
2023-08-03 00:33:47 +0800

    update reference

--- azChallengeFA-Customer/Functions/EventHubTriggerFunction.cs
+++ azChallengeFA-Customer/Functions/EventHubTriggerFunction.cs
@@ -4,13 +4,14 @@ using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.EventGrid;
 using Microsoft.Extensions.Logging;
-using Azure.Messaging.EventGrid;
 using azChallengeFA_Customer.services;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using System.Text;
 using azChallengeFA_Customer.model;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.Cosmos.Core;
 
 namespace azChallengeFA_Customer.Functions
 {
@@ -33,12 +34,39 @@ namespace azChallengeFA_Customer.Functions
             try
             {
                 // Deserialize the binary data to the Customer class
-                var customer = JsonConvert.DeserializeObject<CustomerInfo>(Encoding.UTF8.GetString(eventGridEvent.Data));
+                var eventData = eventGridEvent.Data as JObject;
 
-                if (customer != null && !string.IsNullOrEmpty(customer.FirstName) && !string.IsNullOrEmpty(customer.LastName) && !string.IsNullOrEmpty(customer.Email))
+                if (eventData == null)
                 {
+                    // Convert BinaryData to a string and then deserialize to JObject
+                    var eventDataAsString = Encoding.UTF8.GetString((byte[])eventGridEvent.Data);
+                    eventData = JObject.Parse(eventDataAsString);
+                }
+
+                // Check if eventData contains the necessary properties
+                if (eventData.ContainsKey("FirstName") && eventData.ContainsKey("LastName") && eventData.ContainsKey("Email"))
+                {
+                    // Extract the customer information
+                    string Id = eventData["Id"].ToString();
+                    string firstName = eventData["FirstName"].ToString();
+                    string lastName = eventData["LastName"].ToString();
+                    long birthdayInEpoch = eventData.ContainsKey("BirthdayInEpoch") ? eventData["BirthdayInEpoch"].ToObject<long>() : 0;
+                    string email = eventData["Email"].ToString();
+
+                    // Create a Customer object from the event data
+                    var customer = new CustomerInfo
+                    {
+                        Id = Guid.Parse(Id),
+                        FirstName = firstName,
+                        LastName = lastName,
+                        BirthdayInEpoch = birthdayInEpoch,
+                        Email = email
+                    };
+
+                    // Upsert the customer data into Cosmos DB
                     await _customerService.UpsertCustomerDataAsync(customer);
-                    _logger.LogInformation($"Customer data upserted: {customer.FirstName} {customer.LastName} ({customer.Email})");
+
+                    _logger.LogInformation($"Customer data upserted: {firstName} {lastName} ({email})");
                 }
                 else
                 {
